2bd42815aff44be8e4f6d156c35ec95bea199771
KL_Taekwondo <16424544+kltaekwondo@user.noreply.gitee.com>
2026-06-03 13:02:31 +0800

    切换到纯前端架构
    - 删除 Koole-backend 后端代码
    - 保留前端登录卡片设计（可惜了）
    - 后续将采用纯前端方式开发

--- Koole-backend/src/main/java/com/kldo/koolebackend/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kldo.koolebackend.repository;
-
-import com.kldo.koolebackend.entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
--- Koole-backend/src/main/java/com/kldo/koolebackend/repository/UpdatePostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kldo.koolebackend.repository;
-
-import com.kldo.koolebackend.entity.UpdatePost;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UpdatePostRepository extends JpaRepository<UpdatePost, Long> {
-}
